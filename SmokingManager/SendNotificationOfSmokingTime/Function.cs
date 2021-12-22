f7df135e7d9a0aabceb3da691fe00d759b8ae82a
しゃるしぃ <info@angelbell.jp>
2021-12-23 01:19:30 +0900

    Create instance of LineMessagingClient.

--- SmokingManager/SendNotificationOfSmokingTime/Function.cs
+++ SmokingManager/SendNotificationOfSmokingTime/Function.cs
@@ -1,11 +1,13 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
+using System.IO;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
-
+using Amazon;
 using Amazon.Lambda.Core;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+using Line.Messaging;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
@@ -13,9 +15,26 @@ namespace SendNotificationOfSmokingTime
 {
     public class Function
     {
-        public string FunctionHandler(Input input, ILambdaContext context)
+        public async Task FunctionHandler(Input input, ILambdaContext context)
         {
-            return input.Environment;
+            var secretsJson = await GetSecretAsync("arn:aws:secretsmanager:ap-northeast-1:729870111298:secret:line/SmokingManager/Values-S7sLCm");
+            Console.WriteLine(secretsJson);
+            var secrets = System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, string>>(secretsJson);
+            Console.WriteLine(secrets["ChannelAccessToken"]);
+            using var line = new LineMessagingClient(secrets["ChannelAccessToken"]);
+            Console.WriteLine(secrets[$"{input.Environment}:Recipient"]);
+        }
+
+        public static async ValueTask<string> GetSecretAsync(string secretName)
+        {
+            using var client = new AmazonSecretsManagerClient(RegionEndpoint.GetBySystemName("ap-northeast-1"));
+            var request = new GetSecretValueRequest
+            {
+                SecretId = secretName,
+                VersionStage = "AWSCURRENT"
+            };
+            var response = await client.GetSecretValueAsync(request);
+            return response.SecretString;
         }
 
         public class Input
