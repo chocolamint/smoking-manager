46e6dca5cae1c1dd9618d702538ee2ff5343c861
しゃるしぃ <info@angelbell.jp>
2021-12-23 00:16:38 +0900

    Receive input.

--- SmokingManager/SendNotificationOfSmokingTime/Function.cs
+++ SmokingManager/SendNotificationOfSmokingTime/Function.cs
@@ -1,27 +1,27 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 using Amazon.Lambda.Core;
 
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace SendNotificationOfSmokingTime
 {
     public class Function
     {
-        
-        /// <summary>
-        /// A simple function that takes a string and does a ToUpper
-        /// </summary>
-        /// <param name="input"></param>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        public string FunctionHandler(Input input, ILambdaContext context)
         {
-            return input?.ToUpper();
+            return input.Environment;
+        }
+
+        public class Input
+        {
+            [JsonPropertyName("Env")]
+            public string Environment { get; set; }
         }
     }
 }
